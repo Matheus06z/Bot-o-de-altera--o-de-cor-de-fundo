1b26980dea4c4ccd47e44451560ce10a150120fd
Matheus Gabriel <matheus.gabriel24091@gmail.com>
2022-10-19 21:26:54 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-laranja").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "laranja");
+})
+document.getElementById("btn-vermelho").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "vermelho");
+})
+document.getElementById("btn-roxo").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "roxo");
+})
